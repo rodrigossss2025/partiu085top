d5096fac32cf09680b5d6c22a53be553bf20299f
rodrigossss2025 <rodrigossssss@gmail.com>
2025-12-20 16:22:17 -0300

    chore(debug): add MilhasPage only

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,14 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { RadarPage } from "./pages/RadarPage";
-import { AlertasPage } from "./pages/AlertasPage";
+import { MillasLabPage } from "./pages/MillasLabPage";
+
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<RadarPage />} />
-        <Route path="/alertas" element={<AlertasPage />} />
+        <Route path="/lab-milhas" element={<MillasLabPage />} />
       </Routes>
     </BrowserRouter>
   );
