ab24a935271aeccd8b5a56ca98ffc57493eee127
rodrigossss2025 <rodrigossssss@gmail.com>
2025-12-20 22:27:12 -0300

    feat: restaura navegação BottomNavBar no App

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -6,16 +6,27 @@ import { MillasLabPage } from "./pages/MillasLabPage";
 import { SettingsPage } from "./pages/SettingsPage";
 import { AlertasPage } from "./pages/AlertasPage";
 
+// Importando o componente que estava faltando
+import { BottomNavBar } from "./components/BottomNavBar";
+
 export default function App() {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<RadarPage />} />
-        <Route path="/resultados" element={<ResultsPage />} />
-        <Route path="/lab-milhas" element={<MillasLabPage />} />
-        <Route path="/alertas" element={<AlertasPage />} />
-        <Route path="/config" element={<SettingsPage />} />
-      </Routes>
+      {/* Envolvemos tudo em uma div com flex-col para garantir que
+          o conteúdo não fique "atrás" da barra de navegação
+      */}
+      <div className="flex flex-col min-h-screen bg-slate-900 pb-20">
+        <Routes>
+          <Route path="/" element={<RadarPage />} />
+          <Route path="/resultados" element={<ResultsPage />} />
+          <Route path="/lab-milhas" element={<MillasLabPage />} />
+          <Route path="/alertas" element={<AlertasPage />} />
+          <Route path="/config" element={<SettingsPage />} />
+        </Routes>
+
+        {/* Adicionando a barra de navegação aqui embaixo */}
+        <BottomNavBar />
+      </div>
     </BrowserRouter>
   );
 }
